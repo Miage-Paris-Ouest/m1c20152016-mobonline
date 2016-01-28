aa140435e4c35f873dcfbded3e9171f695faee4d
Florian-MIAGE <florianroy@laposte.net>
2016-01-28 12:44:49 +0100

    Restructuration sql files

---
+++ DATABASE/BDD_INSERT_CUSTOM_TABLES.sql
@@ -0,0 +1,26 @@
+INSERT INTO M1_STATUS VALUES(1,'Super Admin');
+INSERT INTO M1_STATUS VALUES(2,'Admin');
+INSERT INTO M1_STATUS VALUES(3,'Student');
+INSERT INTO M1_STATUS VALUES(4,'Contact');
+
+
+INSERT INTO M1_UNIVERSITIES VALUES(1,'Université Paris Ouest Nanterre La Défense','200 Avenue de la République','http://www.u-paris10.fr/',3715);
+
+
+INSERT INTO M1_PERSONS VALUES(1,'Florian','Roy','florianroy@laposte.net','+33659853307',1,1);
+INSERT INTO M1_PERSONS VALUES(2,'Bruno','Veraldi','bruno.veraldi@gmail.com','+33658018622',1,1);
+INSERT INTO M1_PERSONS VALUES(3,'Adrien','Dos Santos','adriendossantos29@gmail.com','+33675290100',1,1);
+
+
+INSERT INTO M1_USERS VALUES(1,'flo','flo123',1);
+INSERT INTO M1_USERS VALUES(2,'bru','bruno123',2);
+INSERT INTO M1_USERS VALUES(3,'adri','adrien123',3);
+
+
+INSERT INTO M1_BUILDING_TYPES VALUES(1,'Housing');
+INSERT INTO M1_BUILDING_TYPES VALUES(2,'Station');
+INSERT INTO M1_BUILDING_TYPES VALUES(3,'Restoration');
+INSERT INTO M1_BUILDING_TYPES VALUES(4,'Sports Complex');
+INSERT INTO M1_BUILDING_TYPES VALUES(5,'Medical Center');
+INSERT INTO M1_BUILDING_TYPES VALUES(6,'Library');
+INSERT INTO M1_BUILDING_TYPES VALUES(7,'Administration');
