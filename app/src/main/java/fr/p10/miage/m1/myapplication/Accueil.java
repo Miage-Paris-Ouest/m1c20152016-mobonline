ffc1122b2c38804e4582b59656d39874c609ca2c
satan99999 <adriendossantos29@gmail.com>
2016-02-11 12:00:57 +0100

    settings create

--- app/src/main/java/fr/p10/miage/m1/myapplication/Accueil.java
+++ app/src/main/java/fr/p10/miage/m1/myapplication/Accueil.java
@@ -8,10 +8,12 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ImageButton;
+import android.widget.PopupMenu;
 
 public class Accueil extends AppCompatActivity  implements NavigationView.OnNavigationItemSelectedListener {
 
@@ -131,7 +133,8 @@ public class Accueil extends AppCompatActivity  implements NavigationView.OnNavi
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_profile) {
-            return true;
+            Intent intent = new Intent(this, Settings.class);
+            startActivity(intent);
         }
 
         return super.onOptionsItemSelected(item);
