fea6f0e1b448d4e935003977b03442291790383c
Bruno-MIAGE <bru.veraldi@hotmail.fr>
2016-02-11 11:57:27 +0100

    APP: ajout de la solution generique de gestion des listes avec effet de dropdown (ajout des images + et -, ajout du layout list_header), adaptation exemple de how to (how to.java et son activite)

--- app/src/main/java/fr/p10/miage/m1/myapplication/HowTo.java
+++ app/src/main/java/fr/p10/miage/m1/myapplication/HowTo.java
@@ -1,20 +1,74 @@
 package fr.p10.miage.m1.myapplication;
 
+import fr.p10.miage.m1.myapplication.ExpandableList.ExpandableListAdapter;
+
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class HowTo extends AppCompatActivity {
+    private RecyclerView recyclerview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_how_to);
+
+        recyclerview = (RecyclerView) findViewById(R.id.recyclerview);
+        recyclerview.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
+
+        List<ExpandableListAdapter.Item> data = new ArrayList<>();
+
+        data.add(new ExpandableListAdapter.Item(ExpandableListAdapter.HEADER, "INSCRIPTION"));
+        data.add(new ExpandableListAdapter.Item(ExpandableListAdapter.CHILD, "Administrative Register"));
+        data.add(new ExpandableListAdapter.Item(ExpandableListAdapter.CHILD, "Teaching registration"));
+
+        data.add(new ExpandableListAdapter.Item(ExpandableListAdapter.HEADER, "SCHOLARSHIP"));
+        data.add(new ExpandableListAdapter.Item(ExpandableListAdapter.CHILD, "Je sais pas"));
+
+
+        //EXEMPLE D'AJOUT DE CATEGORIE AVEC CHILDS
+/*
+        ExpandableListAdapter.Item places = new ExpandableListAdapter.Item(ExpandableListAdapter.HEADER, "Places");
+        places.invisibleChildren = new ArrayList<>();
+        places.invisibleChildren.add(new ExpandableListAdapter.Item(ExpandableListAdapter.CHILD, "Kerala"));
+        places.invisibleChildren.add(new ExpandableListAdapter.Item(ExpandableListAdapter.CHILD, "Tamil Nadu"));
+        places.invisibleChildren.add(new ExpandableListAdapter.Item(ExpandableListAdapter.CHILD, "Karnataka"));
+        places.invisibleChildren.add(new ExpandableListAdapter.Item(ExpandableListAdapter.CHILD, "Maharashtra"));
+
+        data.add(places);
+        */
+
+        recyclerview.setAdapter(new ExpandableListAdapter(data));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 
     @Override
